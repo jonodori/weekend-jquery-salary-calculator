e208bb03bf4c953e68e59e133a0080b51462442a
Jonathan Palalay <ring5pun@hotmail.com>
2022-05-13 17:57:54 -0500

    appended values to DOM

--- script.js
+++ script.js
@@ -4,5 +4,30 @@ console.log('js loaded!');
 $(document).ready(handleReady)
 
 function handleReady(){
-    console.log('DOM/jquery loaded!');
+    $(document).on('click', '#submit-btn', onSubmit);
+}
+
+function onSubmit(){
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
+    let idNumber = $('#id-number').val();
+    let jobTitle = $('#job-title').val();
+    let AnnualSalary = $('#annual-salary').val();
+
+    $('#salary-table').append(`
+    <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${idNumber}</td>
+                <td>${jobTitle}</td>
+                <td>$${AnnualSalary}</td>
+            </tr>
+    `);
+
+    $('#movie-name').val('');
+    $('#last-name').val('');
+    $('#id-number').val('');
+    $('#job-title').val('');
+    $('#annual-salary').val('');
+
 }
