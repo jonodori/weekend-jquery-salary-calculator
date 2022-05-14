f90cbdd827b8fc87192f2301a0a8e065fb53d7e3
Jonathan Palalay <ring5pun@hotmail.com>
2022-05-14 12:46:35 -0500

    finished calc for totalSalary

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let newEmployee = {
     lastName: $('#last-name').val(),
     idNumber: $('#id-number').val(),
     jobTitle: $('#job-title').val(),
-    AnnualSalary: $('#annual-salary').val()
+    AnnualSalary: Number($('#annual-salary').val())
 }
 
     //empty inputs
@@ -30,21 +30,34 @@ let newEmployee = {
 employees.push(newEmployee);
 
 
+
     $('#salary-table').append(`
     <tr>
                 <td>${newEmployee.firstName}</td>
                 <td>${newEmployee.lastName}</td>
                 <td>${newEmployee.idNumber}</td>
                 <td>${newEmployee.jobTitle}</td>
-                <td>${newEmployee.AnnualSalary}</td>
+                <td>$${newEmployee.AnnualSalary}</td>
             </tr>
     `);
 
-for(let salary of employees){
-    let totalSalary = 0;
-    totalSalary += (salary.AnnualSalary);
-    console.log(totalSalary);
-    }
+calcAnnual();
+
 }
 
+//created a function to do the calculation 
+function calcAnnual(){ 
 
+let totalSalary = 0; // have to create variable outside the loop for calc to work
+
+for(let salary of employees){
+    
+    totalSalary += salary.AnnualSalary;
+    console.log(totalSalary); //test for totalSalary
+
+    let el = $('.Total-Monthly'); //created variable to append and empty 
+    el.empty();
+    el.append(totalSalary);
+
+    }
+}
